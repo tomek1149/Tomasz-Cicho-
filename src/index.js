f93e6a2fe2dc9faf7e55b44ab65fd9c335038c2c
Tomasz Cichon <tomek1149@gmail.com>
2019-12-02 19:57:23 +0100

    finnish home page on mobile

--- src/index.js
+++ src/index.js
@@ -31,3 +31,26 @@ const navSlide = () => {
 }
 
 navSlide();
+
+const socialNav = () => {
+    const socialString = document.querySelector(".social");
+    const navSocial = document.querySelector(".social-icons");
+    const icons = document.querySelectorAll(".icons a i");
+
+
+    socialString.addEventListener('click', () => {
+        navSocial.classList.toggle('social-active');
+
+        icons.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+
+            } else {
+                link.style.animation = `navFadeSocial .5s ease forwards ${index / 7 + 0.4}s`;
+            }
+        })
+
+    });
+}
+
+socialNav();
