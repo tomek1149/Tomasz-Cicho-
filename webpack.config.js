4a911df795ccd6e2960f52f476f7394c17890dce
Tomasz Cichon <tomek1149@gmail.com>
2020-01-03 15:58:32 +0100

    configure webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,19 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
+const buildPath = path.resolve(__dirname, 'dist');
 
 module.exports = {
+    entry: {
+        index: './src/index.js',
+        about: './src/about.js'
+    },
+
+    output: {
+        filename: '[name].[hash:20].js',
+        path: buildPath
+    },
+
     module: {
         rules: [
             {
@@ -13,6 +25,7 @@ module.exports = {
             },
             {
                 test: /\.html$/,
+                exclude: /node_modules/,
                 use: [
                     {
                         loader: "html-loader",
@@ -53,11 +66,19 @@ module.exports = {
     plugins: [
         new HtmlWebPackPlugin({
             template: "./src/index.html",
-            filename: "./index.html"
+            filename: "index.html",
+            inject: true,
+            chunks: ['index'],
+        }),
+        new HtmlWebPackPlugin({
+            template: "./src/about.html",
+            filename: "about.html",
+            inject: true,
+            chunks: ['about']
         }),
         new MiniCssExtractPlugin({
             filename: "[name].css",
             chunkFilename: "[id].css"
         })
-    ]
+    ],
 }
